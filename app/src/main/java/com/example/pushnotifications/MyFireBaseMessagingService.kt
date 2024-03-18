554c63a94287be32ef8cf2de6b9052dc7b00f1ce
Dan Georgas <dgeorgas@students.mchenry.edu>
2024-03-18 14:59:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pushnotifications/MyFireBaseMessagingService.kt
@@ -0,0 +1,20 @@
+package com.example.pushnotifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFireBaseMessagingService: FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        // Handle data payload of FCM messages.
+        if (remoteMessage.data.isNotEmpty()) {
+            // Handle the data message here.
+        }
+
+        // Handle notification payload of FCM messages.
+        remoteMessage.notification?.let {
+            // Handle the notification message here.
+        }
+    }
+}
